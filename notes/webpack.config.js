bf3359d8af115a5e88466d1c5e296ef7bac1800c
peppermintCode <2293420862@qq.com>
2018-11-20 10:54:00 +0800

    update

--- notes/webpack.config.js
+++ notes/webpack.config.js
@@ -7,11 +7,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-    entry:{ //多入口
+    entry:{ // 多入口
         list: './src/js/list.js',
         detial: './src/js/detail.js'
     },
-    output: { //多出口
+    output: { // 多出口
         path: path.resolve(__dirname, './build'),
         filename: '[name].[hash:8].js'
     },
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.css$/,
-                use: [
+                use: [ //从右往左
                     {loader: style-loader},
                     {loader: css-loader},
                 ]
@@ -37,14 +37,14 @@ module.exports = {
         ]
     },
     plugins:[
-        new CleanWebpackPlugin(['./buid']), //每次构建都清空build文件夹
-        new HtmlWebpackPlugin({// 根据模板index,生成detail.html.插入打包后detail.[hash].js
+        new CleanWebpackPlugin(['./buid']), // 每次构建都清空build文件夹
+        new HtmlWebpackPlugin({ // 根据模板index,生成detail.html.插入打包后detail.[hash].js
             filename: 'detail.html',
             template: './src/index.html',
             hash: true,
             chunks: ['detail']
         }),
-        new HtmlWebpackPlugin({// 根据模板index,生成list.html.插入打包后list.[hash].js
+        new HtmlWebpackPlugin({ // 根据模板index,生成list.html.插入打包后list.[hash].js
             filename: 'list.html',
             template: './src/index.html',
             hash: true,
